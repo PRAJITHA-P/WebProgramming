e0a29bc4ea6cec942956348bd1f12c9fb65c2165
PRAJITHA P <74781318+PRAJITHA-P@users.noreply.github.com>
2021-03-07 20:24:07 +0530

    course outcome3

---
+++ CO3/RegSuccess.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<title>Success.php</title>
+<body>
+
+<div style="text-align:left"><h1>Registration Successful </h1></div>
+<br/>
+
+<div style="text-align: left"><a href="Table.php">Details of all users</a></div>
+
+</body>
+</html>
